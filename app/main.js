995f2c24d56a188de9bf53c475f419ad2cf96fc4
meqdad <meqdad.moradi@outlook.com>
2021-10-18 21:48:22 +0200

    responsive toggle menu added

--- app/main.js
+++ app/main.js
@@ -1,27 +1,52 @@
-//////////////////////////
-// toggle the navigation bar
-const headerNav = document.querySelector('.header-nav')
-const headerNavList = document.querySelector('.nav-list')
-const toggler = document.querySelector('#toggler-btn')
+// responsive mobile menu
+const app = {
 
+    init: () => {
+        window.addEventListener('DOMContentLoaded', app.load)
+    },
 
-// toggler navigation
-toggler.addEventListener('click', e => {
-    const currentBtn = e.currentTarget
+    load: () => {
+        app.toggleMenu()
+    },
 
-    if (currentBtn.classList.contains('fa-bars') && !headerNav.classList.contains('active')) {
-        currentBtn.className = 'fas fa-times'
-        headerNav.classList.add('active')
-    } else {
-        currentBtn.className = 'fas fa-bars'
-        headerNav.classList.remove('active')
+    toggleMenu: () => {
+        // get mobile menu elements
+        const headerNav = document.querySelector('.header-nav')
+        const toggler = document.querySelector('#toggler-btn')
+
+        // toggle navigation
+        toggler.addEventListener('click', e => {
+            const currentBtn = e.currentTarget
+
+            if (currentBtn.classList.contains('fa-bars') && !headerNav.classList.contains('active')) {
+                currentBtn.className = 'fas fa-times'
+                headerNav.classList.add('active')
+            } else {
+                currentBtn.className = 'fas fa-bars'
+                headerNav.classList.remove('active')
+            }
+        })
+
+        // toggle nav
+        headerNav.addEventListener('click', e => {
+            if (e.target !== e.currentTarget) return
+
+            toggler.className = 'fas fa-bars'
+            headerNav.classList.remove('active')
+        })
     }
-})
+}
 
-// header nav toggling 
-headerNav.addEventListener('click', e => {
-    if (e.target !== e.currentTarget) return
+app.init()
 
-    toggler.className = 'fas fa-bars'
-    headerNav.classList.remove('active')
-})
+
+
+/////////////////////////
+// blog post comments
+// select all required elements
+
+const blogForm = document.querySelector('#comment-form')
+
+const blogPosts = {
+
+}
